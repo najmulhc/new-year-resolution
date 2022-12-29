c9f50ab4444e79c94dc8aa6e7dc8e902b6732da1
najmulhc <kridi32@outlook.com>
2022-12-29 20:33:25 +0600

    initial release a

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json())
 
 //getting data from database
 const data = JSON.parse(fs.readFileSync(`${__dirname}/data.json`, "utf-8"));
@@ -15,11 +16,20 @@ app.get("/api/v1/", (req, res) => {
   });
 });
 
-app.post("/api/v1/", (req, res) => {
-  const resolution = req.body;
-  data.push(resolution);
-  const dataToWrite = JSON.stringify(data);
-  fs.writeFile(`${__dirname}/data.json`, dataToWrite);
+app.post("/api/v1", (req, res) => {
+  const colors = [
+    "#300449",
+    "#e66a1f",
+    "#fdd02b",
+    "#e42a69",
+    "#3cb64f",
+  ];
+  const newResolution = req.body;
+  data.push({...newResolution, color: colors[Math.ceil(Math.random()*5)]});
+  const stringyFied  = JSON.stringify(data)
+  fs.writeFile(`${__dirname}/data.json`, stringyFied, 'utf-8', err => {
+    console.error(err)
+  })
   res.status(200).json({
     status: "success",
     data,
